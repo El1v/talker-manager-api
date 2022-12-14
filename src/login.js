3d9e78110c98ad1c6139cb04b3db724ec5ea3d7e
Elivelton <eliveltonmn@outlook.com>
2022-12-14 16:30:49 -0300

    Quarto requisito

--- src/login.js
+++ src/login.js
@@ -11,6 +11,50 @@ const generateToken = () => {
   return token;
 };
 
+const validateEmailFormat = (email) => {
+  const validEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  return email.match(validEmail);
+};
+
+const validateEmail = (req, res, next) => {
+  const { body } = req;
+  if (body.email === undefined) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!validateEmailFormat(body.email)) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { body } = req;
+  if (body.password === undefined) {
+    return res
+      .status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (body.password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+// const validateTeam = (req, res, next) => {
+//   const requiredProperties = ["nome", "sigla"];
+//   if (requiredProperties.every((property) => property in req.body)) {
+//     next(); // Chama o próximo middleware
+//   } else {
+//     res.sendStatus(400); // Ou já responde avisando que deu errado
+//   }
+// };
+
 module.exports = {
   generateToken,
+  validateEmail,
+  validatePassword,
 };
--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const login = require("../login");
 
-router.post("/", (req, res) => {
+router.post("/", login.validateEmail, login.validatePassword, (req, res) => {
   // const user = req.body;
   const token = login.generateToken();
   res.status(200).json({ token });
